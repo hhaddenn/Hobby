d2cb479c7d9f1467593d3a2d0fb3c5080bd70815
hhaddenn <hugohaddenn@gmail.com>
2021-07-19 19:02:12 +0100

    Quando adicionamos palavras, o conteudo do ficheiro é todo substituido, erro quando voltamos ao menu apos adicionar palavras

--- Jogos/JogoForca/Jogo_Da_Forca.java
+++ Jogos/JogoForca/Jogo_Da_Forca.java
@@ -2,9 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Jogo_Da_Forca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner ler = new Scanner(System.in);
-        int opcao = 0;
+        int opcao, escolha, random = 0;
         do {
             System.out.println("Lista de Categorias:");
             System.out.println("1- Animais");
@@ -15,8 +15,8 @@ public class Jogo_Da_Forca {
             System.out.println();
             switch (opcao) {
                 case 1:
-                    int escolha = 0;
                     do {
+                        escolha = 0;
                         System.out.println("Animais:");
                         System.out.println("1- Jogar");
                         System.out.println("2- Adicionar palavras");
@@ -26,7 +26,8 @@ public class Jogo_Da_Forca {
                         System.out.println();
                         if (escolha==1) {
                             File f = new File("animais.txt");
-                            Jogar(f);
+                            random = (int)(Math.random() * (f.length() - 1)) + 1;
+                            Jogar(random, f);
                         }
                         if (escolha==2) {
                             File f = new File("animais.txt");
@@ -35,18 +36,50 @@ public class Jogo_Da_Forca {
                     } while (escolha!=10);
                     break;
             
-                default:
+                case 2:
+                    do {
+                        escolha = 0;
+                        System.out.println("Desporto:");
+                        System.out.println("1- Jogar");
+                        System.out.println("2- Adicionar palavras");
+                        System.out.println("10- Sair");
+                        System.out.print("Escolha uma opção -> ");
+                        escolha = ler.nextInt();
+                        System.out.println();
+                        if (escolha==1) {
+                            File f = new File("desporto.txt");
+                            random = (int)(Math.random() * (f.length() - 1)) + 1;
+                            Jogar(random, f);
+                        }
+                        if (escolha==2) {
+                            File f = new File("desporto.txt");
+                            Adicionar(f);
+                        }
+                    } while (escolha!=10);
                     break;
             }
         } while (opcao!=10);
     }
 
-    public static void Jogar(File f){
+    public static void Jogar(int random, File f) throws IOException {
+        Scanner read = new Scanner(f);
 
+        read.close();
     }
 
-    public static void Adicionar(File f){
-
+    public static void Adicionar(File f) throws IOException {
+        FileWriter writer = new FileWriter(f);
+        Scanner sc = new Scanner(System.in);
+        String word;
+        System.out.println("O ficheiro irá fechar quando não escrever nada");
+        System.out.println("Escreva as palavras que quer adicionar:");
+        word = sc.nextLine();
+        while (word.length()>0) {
+            writer.write(word.toUpperCase());
+            word = sc.nextLine();
+        }
+        writer.close();
+        sc.close();
     }
 
 }
