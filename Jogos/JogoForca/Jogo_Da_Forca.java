59397abc102994ddb1f6ae750a0a828ab3b217ee
hhaddenn <hugohaddenn@gmail.com>
2021-07-20 00:35:26 +0100

    Problema na parte de guardar as palavras no array

--- Jogos/JogoForca/Jogo_Da_Forca.java
+++ Jogos/JogoForca/Jogo_Da_Forca.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Jogo_Da_Forca {
     public static void main(String[] args) throws IOException {
         Scanner ler = new Scanner(System.in);
-        int opcao, escolha, random = 0;
+        int opcao, escolha = 0;
+        String palavra;
         do {
             System.out.println("Lista de Categorias:");
             System.out.println("1- Animais");
@@ -26,8 +27,7 @@ public class Jogo_Da_Forca {
                         System.out.println();
                         if (escolha==1) {
                             File f = new File("animais.txt");
-                            random = (int)(Math.random() * (f.length() - 1)) + 1;
-                            Jogar(random, f);
+                            palavra = Palavra(f);
                         }
                         if (escolha==2) {
                             File f = new File("animais.txt");
@@ -48,8 +48,7 @@ public class Jogo_Da_Forca {
                         System.out.println();
                         if (escolha==1) {
                             File f = new File("desporto.txt");
-                            random = (int)(Math.random() * (f.length() - 1)) + 1;
-                            Jogar(random, f);
+                            palavra = Palavra(f);
                         }
                         if (escolha==2) {
                             File f = new File("desporto.txt");
@@ -61,21 +60,44 @@ public class Jogo_Da_Forca {
         } while (opcao!=10);
     }
 
-    public static void Jogar(int random, File f) throws IOException {
+    public static String Palavra(File f) throws IOException {
         Scanner read = new Scanner(f);
-
+        int random = 0;
+        int linhas = 0;
+        String word;
+        int i = 0;
+        while (read.hasNext()) {
+            read.nextLine();
+            linhas++;
+        }
+        String a[] = new String[linhas];
+        //há um problema nesta parte
+        /*while (read.hasNext()) {
+            a[i] = read.nextLine();
+            System.out.println(a[i]);
+            if (i<a.length) {
+                i++;
+            }
+        }*/
+        random = (int)(Math.random() * (a.length - 0)) + 0;
+        word = a[random];
         read.close();
+        return word;
     }
 
     public static void Adicionar(File f) throws IOException {
-        FileWriter writer = new FileWriter(f);
+        FileWriter writer = new FileWriter(f, true);
         Scanner sc = new Scanner(System.in);
         String word;
         System.out.println("O ficheiro irá fechar quando não escrever nada");
         System.out.println("Escreva as palavras que quer adicionar:");
         word = sc.nextLine();
+        if (word!="") {
+            writer.write("\n");
+        }
         while (word.length()>0) {
             writer.write(word.toUpperCase());
+            writer.write("\n");
             word = sc.nextLine();
         }
         writer.close();
