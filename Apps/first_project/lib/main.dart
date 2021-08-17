27ce4f5fdb8209f72d9757064a40b2aad7e9a9b1
hhaddenn <hugohaddenn@gmail.com>
2021-08-17 21:53:20 +0100

    Apps atualizadas

--- Apps/first_project/lib/main.dart
+++ Apps/first_project/lib/main.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
 
-//void main() => runApp(MyApp());
-void main(){
-  runApp(MyApp());
+import './question.dart';
+import './answer.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget{
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Scaffold(
+  Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'What\'s your favourite color?',
+        'answers': ['Blue', 'Red', 'Green', 'Yellow'],
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': ['Cat', 'Dog', 'Octopus', 'Otter'],
+      },
+      {
+        'questionText': 'Who\'s your favourite actor?',
+        'answers': ['Johnny Depp', 'Leonardo DiCaprio', 'Tom Hanks', 'George Clooney'],
+      },
+    ];
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('Default Text!'),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+          ],
+        ),
       ),
     );
   }
